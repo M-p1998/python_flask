4e9d8ebbe9b7e1698033fd4fb752252e407ff924
M_p <skyusmpp@gmail.com>
2022-06-24 16:49:35 -0700

    updates

--- Flask+MySQL/database_connection/first_flask_MySQL/server.py
+++ Flask+MySQL/database_connection/first_flask_MySQL/server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+
+from flask import Flask, render_template, redirect, request
 # import the class from friend.py
 from friend import Friend
 app = Flask(__name__)
@@ -8,6 +9,18 @@ def index():
     friends = Friend.get_all()
     print(friends)
     return render_template("index.html", my_friends=friends)
+
+@app.route("/create_friend", methods=["post"])
+def create_friend():
+    data={
+        "fname": request.form["fname"],
+        "lname":request.form["lname"],
+        "occ": request.form["occ"]
+    }
+    
+    Friend.save(data)
+    print(request.form)
+    return redirect("/")
             
 if __name__ == "__main__":
     app.run(debug=True)
